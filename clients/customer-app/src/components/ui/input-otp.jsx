223cfa635cfd56858ea0649ef770db27db909c5f
TBMAlAmin <talalalamin66@gmail.com>
2026-04-08 21:22:32 +0300

    Fix OTP code visibility

--- clients/customer-app/src/components/ui/input-otp.jsx
+++ clients/customer-app/src/components/ui/input-otp.jsx
@@ -26,7 +26,7 @@ const InputOTPSlot = React.forwardRef(({ index, className, ...props }, ref) => {
     (<div
       ref={ref}
       className={cn(
-        "relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
+        "relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm text-slate-900 shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
         isActive && "z-10 ring-1 ring-ring",
         className
       )}
@@ -35,7 +35,7 @@ const InputOTPSlot = React.forwardRef(({ index, className, ...props }, ref) => {
       {hasFakeCaret && (
         <div
           className="pointer-events-none absolute inset-0 flex items-center justify-center">
-          <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
+          <div className="h-4 w-px animate-caret-blink bg-slate-900 duration-1000" />
         </div>
       )}
     </div>)
